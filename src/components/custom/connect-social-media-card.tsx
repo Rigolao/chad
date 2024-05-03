d1f0d3b7bc71d05afcc2ea6c075d9c15ffe77c30
mrssouza <mrssouza@unaerp.br>
2024-05-03 11:12:51 -0300

    Feat: Info de conexão.

--- src/components/custom/connect-social-media-card.tsx
+++ src/components/custom/connect-social-media-card.tsx
@@ -1,6 +1,8 @@
-import {Card, CardContent} from "@/components/ui/card.tsx";
+import {Card, CardContent, CardHeader} from "@/components/ui/card.tsx";
 import {Button} from "@/components/ui/button.tsx";
 import {ElementType} from "react";
+import {InfoIcon} from "lucide-react";
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip.tsx";
 
 interface ConnectSocialMediaCardProps {
     label: string
@@ -9,8 +11,20 @@ interface ConnectSocialMediaCardProps {
 
 export function ConnectSocialMediaCard({label, icon: Icon}: ConnectSocialMediaCardProps) {
     return (
-        <Card className="flex flex-col w-full md:min-w-72 items-center text-center justify-center pt-6">
-            <CardContent className="flex flex-col items-center gap-3">
+        <Card className="flex flex-col w-full min-w-72 items-center text-center justify-center pt-6">
+            <CardContent className="flex w-full flex-col items-center gap-2">
+                <div className="w-max self-end">
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <InfoIcon className="h-5 w-5"/>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                Informações sobre a conexão
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                </div>
                 <Icon className="h-[3rem] w-[3rem] md:size-8"/>
                 {label}
                 <Button variant="default">
